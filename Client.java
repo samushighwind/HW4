1e98a33d0ca064e16727325238b6839ff05a9368
Ben Wiley <samushighwind@gmail.com>
2015-03-22 22:30:57 -0400

    Got rid of Client.class

--- Client.java
+++ Client.java
@@ -16,6 +16,8 @@ public class Client {
 	final static String INPUT_FILE = "spring-2015.csv";
 	final static String OUTPUT_FILE_1 = "constraint1.txt";
 	final static String OUTPUT_FILE_2 = "constraint2.txt";
+	final static String OUTPUT_FILE_3 = "constraint3.txt";
+	final static String OUTPUT_FILE_4 = "constraint4.txt";
 	
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 		
@@ -39,15 +41,17 @@ public class Client {
 		}
 		scan.close();
 		
-		get_constraint_1(requests);
-		get_constraint_2(requests);
+		getConstraint1(requests);
+		getConstraint2(requests);
+        getConstraint3(requests);
+		getConstraint4(requests);
 	}
 	
 	/*
 	 * Writes the 2nd constraint to a file. This constraint ensures that courses do not exceed the course ceiling. 
 	 * Courses with a ceiling of 0 (i.e. an independent study) are not included in the constraint.
 	 */
-	private static void get_constraint_2(List<Request> requests) throws FileNotFoundException, UnsupportedEncodingException {
+	private static void getConstraint2(List<Request> requests) throws FileNotFoundException, UnsupportedEncodingException {
 		System.out.println("Creating constraint 2: ensure courses do not exceed ceilings...");
 		PrintWriter writer = new PrintWriter(OUTPUT_FILE_2, "UTF-8");
 		Set<Integer> requestUsed = new HashSet<Integer>();
@@ -74,7 +78,7 @@ public class Client {
 		System.out.println("Done with constraint 2. Written to file " + OUTPUT_FILE_2);
 	}
 
-	private static void get_constraint_1(List<Request> requests) throws UnsupportedEncodingException, FileNotFoundException {
+	private static void getConstraint1(List<Request> requests) throws UnsupportedEncodingException, FileNotFoundException {
 		System.out.println("Creating constraint 1: ensure student is not placed in the same course more than once...");
 		PrintWriter writer = new PrintWriter(OUTPUT_FILE_1, "UTF-8");
 	
@@ -106,9 +110,51 @@ public class Client {
     
     private static void getConstraint3(List<Request> requests) throws FileNotFoundException, UnsupportedEncodingException {
         System.out.println("Creating constraint 3: ensure student is not placed in more than 4 courses...");
-		PrintWriter writer = new PrintWriter(OUTPUT_FILE_1, "UTF-8");
+		PrintWriter writer = new PrintWriter(OUTPUT_FILE_3, "UTF-8");
         
+        Set<Integer> requestUsed = new HashSet<Integer>();
+        for(Request i:requests) {
+            int curr_id = i.getId();
+			String curr = "";
+			for(Request j:requests){
+				if(curr_id==j.getId()){//if same student's request
+					if(!requestUsed.contains(j.getVarNum()) && i!=j){
+						curr+="+1 x" + j.getVarNum() + " ";
+						requestUsed.add(j.getVarNum());
+					}
+				}
+			}
+			if(curr.length()!=0){
+				curr += "+1 x" + i.getVarNum() + " ";
+				curr += "<= 4;";
+				requestUsed.add(i.getVarNum());
+				writer.println(curr);
+			}
+            
+        }
+        writer.close();
+        System.out.println("Done with constraint 3. Written to file " + OUTPUT_FILE_4);
+    }
+    
+    private static void getConstraint4(List<Request> requests) throws FileNotFoundException, UnsupportedEncodingException {        System.out.println("Creating constraint 4: minimize rank of all fulfilled requests...");
+		PrintWriter writer = new PrintWriter(OUTPUT_FILE_4, "UTF-8");
         
+        String s = "min: ";
+        for(Request i:requests) {
+			s += "+" + rank(i) +" x" + i.getVarNum() + " ";
+        }
+        writer.println(s);
+        
+        writer.close();
+        System.out.println("Done with constraint 4. Written to file " + OUTPUT_FILE_4);
+    }
+    
+    private static int rank(Request req) {
+        if(req.getBranch() == 1) {
+            return req.getTree();
+        } else if(req.getTree() != 4) {
+            return req.getBranch() % 2 + 1;
+        } else return req.getBranch();
     }
 
 }
